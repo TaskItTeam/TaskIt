d2f89328117d1162803f166cee146e6df0b1e1b4
Samkelisiwe <smklsw@proton.me>
2024-02-13 06:58:35 +0200

    feat: setup infrastructure
    * Setup AWS RDS

---
+++ infra/variables.tf
@@ -0,0 +1,11 @@
+variable "username" {
+  description = "Database username"
+  type = string
+  sensitive = true
+}
+
+variable "password" {
+  description = "Database password"
+  type = string
+  sensitive = true
+}
