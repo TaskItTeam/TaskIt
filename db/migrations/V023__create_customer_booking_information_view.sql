a7da692b88ec08f513e5cfad52c030afb5c7b0c9
Walter Nkosinathi Magagula <walter.magagula@bbd.co.za>
2024-02-15 16:15:31 +0200

    add a view

---
+++ db/migrations/V023__create_customer_booking_information_view.sql
@@ -0,0 +1,26 @@
+CREATE VIEW customer_booking_information 
+AS 
+SELECT 
+	customers.first_name AS customer_first_name,
+	customers.last_name AS customer_last_name,
+	customers.email AS customer_email,
+	customers.phone_number AS customer_phone_number,
+	addresses.street_address AS street_address,
+	addresses.city AS city,
+	workers.first_name AS worker_first_name,
+	workers.last_name AS worker_last_name,
+	tasks.task_name AS task_name,
+    calculate_booking_duration(booking_id) AS task_duration,
+	statuses.name AS booking_status,
+	bookings.booking_date AS booking_date,
+	calculate_booking_price(booking_id) AS booking_cost
+FROM
+	bookings
+	INNER JOIN customers USING (customer_id)
+	INNER JOIN addresses USING (address_id)
+	INNER JOIN booking_workers USING (booking_id)
+	INNER JOIN workers USING (worker_id)
+	INNER JOIN booking_tasks USING (booking_id)
+	INNER JOIN tasks USING (task_id)
+	INNER JOIN statuses USING (status_id)
+	INNER JOIN prices USING (price_id);
