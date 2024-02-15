d904ee15d2ca9c1d49f7f8493f9867113cf23cf7
Cameron Zwane <159318912+cameron-zwane@users.noreply.github.com>
2024-02-15 21:31:00 +0200

    Fixed functions (#22)

--- db/migrations/V011__function_calculate_booking_cost.sql
+++ db/migrations/V011__function_calculate_booking_cost.sql
@@ -6,7 +6,15 @@ BEGIN
     SELECT SUM(p.cost)
     INTO total_price
     FROM task_it.booking_tasks bt
-    JOIN task_it.prices p ON bt.task_id = p.task_id
+    JOIN (
+        SELECT task_id, cost
+        FROM task_it.prices p1
+        WHERE (p1.task_id, p1.date_added) IN (
+            SELECT p2.task_id, MAX(p2.date_added)
+            FROM task_it.prices p2
+            GROUP BY p2.task_id
+        )
+    ) p ON bt.task_id = p.task_id
     WHERE bt.booking_id = booking_id_param;
 
     RETURN total_price;
--- db/migrations/V012__function_create_new_booking.sql
+++ db/migrations/V012__function_create_new_booking.sql
@@ -1,22 +1,26 @@
-CREATE OR REPLACE PROCEDURE task_it.create_new_booking(
-    customer_id integer, 
-    address_id int, 
-    status_id int, 
-    booking_date timestamp, 
-    tasks_ids integer[])
+[21:19] Devon Yeung
+CREATE OR REPLACE PROCEDURE task_it.create_new_booking(IN customer_id integer, IN address_id integer, IN status_id integer, IN booking_date timestamp without time zone, IN tasks_ids integer[])
 LANGUAGE plpgsql
-AS $$
+AS $procedure$
 DECLARE
-    booking_id integer;
+    booking_id_param integer;
     task_id integer;
 BEGIN
     INSERT INTO Bookings (customer_id, address_id, status_id, booking_date)
     VALUES (customer_id, address_id, status_id, booking_date)
-    RETURNING booking_id INTO booking_id;
-
-    FOREACH task_id IN ARRAY tasks_ids LOOP 
-        INSERT INTO Booking_Tasks (booking_id, task_id) 
-        VALUES (booking_id, task_id);
-    END LOOP;
+    RETURNING booking_id INTO booking_id_param;
+ 
+	SELECT booking_id INTO booking_id_param
+    FROM Bookings
+    ORDER BY booking_id DESC
+    LIMIT 1;
+   
+    if tasks_ids is not null then
+    	FOREACH task_id IN ARRAY tasks_ids LOOP
+        	INSERT INTO Booking_Tasks (booking_id, task_id)
+        	VALUES (booking_id_param, task_id);
+    	END LOOP;
+    end if;
 END;
-$$;
+$procedure$
+;
