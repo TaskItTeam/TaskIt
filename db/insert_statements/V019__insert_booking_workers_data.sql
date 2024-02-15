37e257c72a852c4493c4a6a2d4d99deae0e9ea1e
cameron zwane <cameron.cohen@bbd.co.za>
2024-02-15 15:10:13 +0200

    Fixed the folder structure to match flyway expected.

--- db/insert_statements/V019__insert_booking_workers_data.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO task_it.booking_workers (booking_id, worker_id)
-VALUES
-	(1, 1),
-	(2, 2),
-	(3, 3),
-	(4, 4),
-	(5, 5),
-	(6, 6);
---
+++ db/migrations/V022__insert_booking_workers_data.sql
@@ -0,0 +1,8 @@
+INSERT INTO task_it.booking_workers (booking_id, worker_id)
+VALUES
+	(1, 1),
+	(2, 2),
+	(3, 3),
+	(4, 4),
+	(5, 5),
+	(6, 6);
